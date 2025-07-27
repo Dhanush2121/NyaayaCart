f4c5fcde8371be9d5adff65848dacdb0b48b390e
Javvadhi Dhanush <dhanushj217@gmail.com>
2025-07-27 20:35:27 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://nyaaya-cart.vercel.app',
   credentials: true,
 }));
 // app.use(cookieParser());
